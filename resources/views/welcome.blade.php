81d5158ace6b429576398808bfdcde2bfd9ca552
Joshua Oluwajare Faloye <dejavaproglife@gmail.com>
2021-05-09 22:23:01 +0100

    front end

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -115,9 +115,50 @@
                             </figure>
                        </div>
                         <div class="col-12 col-sm-6 d-none d-sm-block d-flex mt-5 justify-content-center">
-                            <figure class="figure">
-                                <img src="{{ asset('storage/img/textbook.png') }}" class="figure-img img-fluid rounded" width="100%" alt="{{ config('app.name') }} School logo" />
-                            </figure>
+                            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                                <div class="carousel-indicators">
+                                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                </div>
+                                <div class="carousel-inner">
+                                  <div class="carousel-item active">
+                                    <figure class="figure">
+                                        <img src="{{ asset('storage/img/textbook.png') }}" class="figure-img img-fluid rounded w-100" alt="{{ config('app.name') }} School logo" />
+                                    </figure>
+                                    <div class="carousel-caption d-none d-md-block">
+                                      <h5>First slide label</h5>
+                                      <p>Some representative placeholder content for the first slide.</p>
+                                    </div>
+                                  </div>
+                                  <div class="carousel-item">
+                                    <figure class="figure">
+                                        <img src="{{ asset('storage/img/textbook.png') }}" class="figure-img img-fluid rounded w-100" alt="{{ config('app.name') }} School logo" />
+                                    </figure>
+                                    <div class="carousel-caption d-none d-md-block">
+                                      <h5>Second slide label</h5>
+                                      <p>Some representative placeholder content for the second slide.</p>
+                                    </div>
+                                  </div>
+                                  <div class="carousel-item">
+                                    <figure class="figure">
+                                        <img src="{{ asset('storage/img/textbook.png') }}" class="figure-img img-fluid rounded w-100" alt="{{ config('app.name') }} School logo" />
+                                    </figure>
+                                    <div class="carousel-caption d-none d-md-block">
+                                      <h5>Third slide label</h5>
+                                      <p>Some representative placeholder content for the third slide.</p>
+                                    </div>
+                                  </div>
+                                </div>
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                  <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                  <span class="visually-hidden">Next</span>
+                                </button>
+                              </div>
                         </div>
                    </div>
                </div>
@@ -129,11 +170,12 @@
                 <div class="row">
                     <div class="col-12 col-col-sm-6 col-md-8">
                         <div class="body-text">
-                            <i class="fa fa-calendar-alt"></i>  History
+                            {{ config('app.name') }}, Ilaro
                         </div>
+                        <h5 class="text-secondary">We are ranked second to none...</h5>
                         <div class="main-underline"></div>
                         <p class="text-justify lead">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. A porro ipsum numquam, sequi pariatur eos accusamus sunt veniam error placeat aspernatur aut eius reprehenderit recusandae. Distinctio recusandae est blanditiis eaque. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum, qui quam dignissimos porro, excepturi molestiae sequi pariatur aliquid expedita ab, asperiores harum. Praesentium provident veritatis impedit aperiam numquam nihil veniam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos delectus incidunt optio ex sit voluptates, voluptate vitae cupiditate numquam blanditiis commodi 
+                            Welcome to the world of {{ config('app.name') }}, Ilaro. The institution was established by law on {{ date('M, d, Y') }} and opened her gates to the public on October 26, 1999. The Institution has since then acquired the reputation of a quintessential institution educating and training students across her group levels. 
                         </p>
                     </div>
                     <div class="col-12 col-col-sm-6 col-md-4 text-center">
@@ -147,23 +189,38 @@
                         </p>
                     </div>
                 </div>
-                <div class="row mt-5">
-                    <div class="col-12">
-                        <div class="body-text">
-                            <i class="fa fa-award"></i> Achievements
+            </div>
+            <div class="text-white p-5">
+                <div class="container">
+                    {{-- <div class="row">
+                        <div class="col-12">
+                            <div class="body-text">
+                                <i class="fa fa-award"></i> Achievements
+                            </div>
                         </div>
-                        <div class="main-underline"></div>
-                    </div>
-                </div>
-                <div class="row mb-5">
-                    <div class="col-12">
-                        <div class="lead">
-                            <div class="row">
-                                <div class="col-12 col-sm-6 col-md-4">
-
-                                </div>
-                                <div class="col-12 col-sm-6 col-md-8">
-                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla, ipsum dolorem natus reprehenderit suscipit dolore voluptatibus aliquid quam quod illo itaque nostrum recusandae rem explicabo, voluptatem quas quo! At, mollitia.
+                    </div> --}}
+                    <div class="underline mb-5"></div>
+                    <div class="row mb-5">
+                        <div class="col-12">
+                            <div class="lead">
+                                <div class="row">
+                                    <div class="col-12 col-sm-6 col-md-4">
+                                        <ul style="margin: 0; list-style-type:none;">
+                                            <li class="lead text-secondary"><span class="icon achievement"></span> Lorem ipsum, dolor sit amet consectetur quaerat officia rem. Sint recusandae illo repudiandae pariatur.</li>
+                                            <li class="mt-3 lead text-secondary"><span class="icon achievement"></span> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi dolores atque nihil cumque est voluptate maiores ratione ab!</li>
+                                            <li class="mt-3 lead text-secondary"><span class="icon achievement"></span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, in quibusdam. Aliquid, autem reprehenderit </li>
+                                            </ul>
+                                    </div>
+                                    <div class="col-12 col-sm-6 col-md-4">
+                                        <ul style="margin: 0; list-style-type:none;">
+                                            <li class="lead text-secondary"><span class="icon achievement"></span> Lorem ipsum, dolor sit amet consectetur quaerat officia rem. Sint recusandae illo repudiandae pariatur.</li>
+                                            <li class="mt-3 lead text-secondary"><span class="icon achievement"></span> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi dolores atque nihil cumque est voluptate maiores ratione ab!</li>
+                                            <li class="mt-3 lead text-secondary"><span class="icon achievement"></span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, in quibusdam. Aliquid, autem reprehenderit </li>
+                                            </ul>
+                                    </div>
+                                    <div class="col-12 col-sm-6 col-md-4 text-center mt-5">
+                                        <i class="fa fa-award text-purple" style="font-size: 10rem;"></i>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -180,7 +237,7 @@
                 </div>
             </div>
             <div class="mt-5"></div>
-            <div style="background-color: #8a12b9;" class="text-white p-5">
+            <div class="text-white p-5 bg-purple">
                 <div class="container">
                     <div class="row">
                         <div class="col-12 mb-5">
