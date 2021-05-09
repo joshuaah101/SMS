a2e3964c504983b8ad9a842de5ac358a0cbe8fb6
Joshua Oluwajare Faloye <dejavaproglife@gmail.com>
2021-05-10 00:03:37 +0100

    front end

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,9 @@
         <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
         
         <!-- Scripts -->
+        {{-- <script src="{{ asset('js/jquery.min.js') }}"></script>
+        <script src="{{ asset('js/bootstrap.bundel.min.js') }}" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script> --}}
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body>
@@ -91,11 +94,11 @@
                 </div>
             </div>
         </nav>
-        <header class="mb-5">
+        <header>
             <div class="banner bg-white">
                <div class="container">
                    <div class="row">
-                       <div class="col-12 col-sm-6 lead" style="margin-top: 4%;">
+                       <div class="col-12 col-sm-5 lead">
                            <div class="header-text">
                                Welcome
                            </div>
@@ -114,51 +117,17 @@
                                 </figcaption>
                             </figure>
                        </div>
-                        <div class="col-12 col-sm-6 d-none d-sm-block d-flex mt-5 justify-content-center">
-                            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-                                <div class="carousel-indicators">
-                                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                                </div>
+                        <div class="col-12 col-sm-7 d-none d-sm-block d-flex justify-content-center">
+                            <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="5000">
                                 <div class="carousel-inner">
                                   <div class="carousel-item active">
-                                    <figure class="figure">
-                                        <img src="{{ asset('storage/img/textbook.png') }}" class="figure-img img-fluid rounded w-100" alt="{{ config('app.name') }} School logo" />
-                                    </figure>
-                                    <div class="carousel-caption d-none d-md-block">
-                                      <h5>First slide label</h5>
-                                      <p>Some representative placeholder content for the first slide.</p>
-                                    </div>
+                                    <img src="{{ asset('storage/img/textbook.png') }}" class="img-fluid rounded w-100" alt="{{ config('app.name') }} School logo" />
                                   </div>
                                   <div class="carousel-item">
-                                    <figure class="figure">
-                                        <img src="{{ asset('storage/img/textbook.png') }}" class="figure-img img-fluid rounded w-100" alt="{{ config('app.name') }} School logo" />
-                                    </figure>
-                                    <div class="carousel-caption d-none d-md-block">
-                                      <h5>Second slide label</h5>
-                                      <p>Some representative placeholder content for the second slide.</p>
-                                    </div>
-                                  </div>
-                                  <div class="carousel-item">
-                                    <figure class="figure">
-                                        <img src="{{ asset('storage/img/textbook.png') }}" class="figure-img img-fluid rounded w-100" alt="{{ config('app.name') }} School logo" />
-                                    </figure>
-                                    <div class="carousel-caption d-none d-md-block">
-                                      <h5>Third slide label</h5>
-                                      <p>Some representative placeholder content for the third slide.</p>
-                                    </div>
+                                    <img src="{{ asset('storage/img/parallax1.jpg') }}" class="img-fluid rounded w-100" alt="{{ config('app.name') }} School logo" />
                                   </div>
                                 </div>
-                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                  <span class="visually-hidden">Previous</span>
-                                </button>
-                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                  <span class="visually-hidden">Next</span>
-                                </button>
-                              </div>
+                            </div>
                         </div>
                    </div>
                </div>
