dd9389d203af5198a549f28faff7a328e633f091
Joshua Oluwajare Faloye <dejavaproglife@gmail.com>
2021-05-09 22:23:01 +0100

    front end

--- resources/views/auth/partials/notification.blade.php
+++ resources/views/auth/partials/notification.blade.php
@@ -1,3 +1,3 @@
-<marquee style="background-color: #8a12b9; color: #fff;" class="p-2">
+<marquee class="p-2 bg-purple text-white">
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, voluptas, dolorem eaque error cupiditate voluptatibus placeat culpa dicta excepturi aut, illo pariatur consequatur sit deleniti magni sunt at distinctio alias.
 </marquee>
