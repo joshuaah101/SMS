58a5479389b0341538172735fb31d15361a568d5
Adenekan Martin <37469720+doubleprincez@users.noreply.github.com>
2021-09-11 22:02:40 +0100

    Styling Notification Blade

--- Modules/Announcement/Database/Migrations/2021_09_11_023509_create_announcements_table.php
+++ Modules/Announcement/Database/Migrations/2021_09_11_023509_create_announcements_table.php
@@ -15,7 +15,13 @@ class CreateAnnouncementsTable extends Migration
     {
         Schema::create('announcements', function (Blueprint $table) {
             $table->id();
-
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->longText('description')->nullable();
+            $table->string('url')->nullable();
+            $table->string('class')->nullable();
+            $table->boolean('active')->nullable()->default(false);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
